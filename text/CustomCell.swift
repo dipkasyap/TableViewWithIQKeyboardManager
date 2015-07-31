33a516c8336eea9a1da57f1e35e5bc8cc0802da3
pr0gramm3r8hai <dpd.ghimire@gmail.com>
2015-07-31 10:31:53 +0545

    first commit!

---
+++ text/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  text
+//
+//  Created by apple on 7/30/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
